1458c729af71be5cb8ffe1a85b3b2d35a2284c61
Axpeykie <3365445989@qq.com>
2021-09-28 09:22:07 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 681d979810703b063abdb19fb41a04f06514737b

---
+++ assets/index.5381f087.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.ead510f6.js";import{b as e,o}from"./vendor.3d1e9ab3.js";var a=r({},[["render",function(r,a){return o(),e("div",null,"login")}]]);export{a as default};
--- assets/index.f5960a71.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.7d7ade25.js";import{b as e,o}from"./vendor.3d1e9ab3.js";var a=r({},[["render",function(r,a){return o(),e("div",null,"login")}]]);export{a as default};
